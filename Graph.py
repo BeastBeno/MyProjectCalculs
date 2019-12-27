5844e893b6bf6ee564029db76b02235b2612aba4
BeastBeno <awoussidietzbenony@gmail.com>
2019-12-27 03:43:32 -0500

    Reussite des deux  courbes de temps et fin du developpement des module pour les calcul

--- Graph.py
+++ Graph.py
@@ -1,5 +1,6 @@
 import csv
 import xlrd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -38,6 +39,29 @@ plt.plot(X, Y2, "o-", label="Actual marks")
 plt.title("My session's marks")
 plt.xlabel("Evaluation number")
 plt.ylabel("Evaluation marks in percent %")
+plt.legend()
+plt.show()
+
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+y = np.array([3, 3, 3, 4, 6, 4, 4, 4, 4, 6, 4, 4, 4, 6, 6])
+
+# Calcul of difficulty degree
+difficulty = 0
+for i in range(len(Y)):
+    difficulty += Y[i] - Y2[i]
+difficulty = (difficulty / (rows-1))
+# creation of the new graph depends on the difficulties
+print(difficulty)
+y2 = []
+for i in range(0, len(y)):
+    y2 += [y[i] + (difficulty * y[i])/100]
+    print(y2[i])
+
+plt.plot(x, y, "*:", label="Session's week")
+plt.plot(x, y2, "o-", label="New week")
+plt.title("My session works' hours")
+plt.xlabel("Week")
+plt.ylabel("Hours")
 plt.legend()
 plt.show()
 
